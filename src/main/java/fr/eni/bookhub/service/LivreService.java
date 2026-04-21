bba9d717c8d0903f6bb24fc1b0b167b2ebb9edb9
kimpatingre <kim.patingre@ekolis-eu.com>
2026-04-22 00:09:48 +0200

    externalisation du mapper

--- src/main/java/fr/eni/bookhub/service/LivreService.java
+++ src/main/java/fr/eni/bookhub/service/LivreService.java
@@ -4,6 +4,9 @@ import fr.eni.bookhub.controller.dto.LivreDTO;
 import fr.eni.bookhub.entity.Auteur;
 import fr.eni.bookhub.entity.Genre;
 import fr.eni.bookhub.entity.Livre;
+import fr.eni.bookhub.exception.GenresNonCorrespondantException;
+import fr.eni.bookhub.exception.LivreDejaExistantException;
+import fr.eni.bookhub.mapper.LivreMapper;
 import fr.eni.bookhub.repository.LivreRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotNull;
@@ -13,10 +16,14 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
+import static fr.eni.bookhub.utils.TextFormatter.isbnFormatteur;
+
 @Validated
 @Service
 @RequiredArgsConstructor
 public class LivreService {
+    private final LivreMapper livreMapper;
+
     private final AuteurService auteurService;
     private final GenreService genreService;
     private final EtatService etatService;
@@ -25,22 +32,21 @@ public class LivreService {
 
     @Transactional
     public void ajoutLivre(@NotNull LivreDTO livreDTO) {
-        Livre livre = mapDTOToLivre(livreDTO);
+
+        if (livreRepository.findByIsbn(isbnFormatteur(livreDTO.isbn())).isPresent()) {
+            throw new LivreDejaExistantException(livreDTO.isbn());
+        }
+
+        Livre livre = livreMapper.toEntity(livreDTO);
         List<Auteur> auteurs = auteurService.retrouverAuteurs(livreDTO.auteurs());
         livre.setAuteurs(auteurs);
         List<Genre> genres = genreService.retrouverGenres(livreDTO.genres());
+        if (genres.size() != livreDTO.genres().size())
+            throw new GenresNonCorrespondantException("");
+
         livre.setGenres(genres);
         livre.setEtat(etatService.retrouveEtat(livreDTO.idEtat()));
-        livreRepository.save(livre);
-    }
 
-    private Livre mapDTOToLivre(@NotNull LivreDTO livreDTO) {
-        Livre livre = new Livre();
-        livre.setIsbn(livreDTO.isbn());
-        livre.setTitre(livreDTO.titre());
-        livre.setDateDeParution(livreDTO.dateDeParution());
-        livre.setSynopsis(livreDTO.synopsis());
-        livre.setUrlImage(livreDTO.urlImage());
-        return livre;
+        livreRepository.save(livre);
     }
 }
