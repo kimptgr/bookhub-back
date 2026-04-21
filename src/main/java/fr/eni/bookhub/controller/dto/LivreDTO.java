c0f0e4bf9630948142508fa690ce0deeb6b01c3c
kimptgr <patingrekim@gmail.com>
2026-04-21 16:58:46 +0200

    :lipstick:

--- src/main/java/fr/eni/bookhub/controller/dto/LivreDTO.java
+++ src/main/java/fr/eni/bookhub/controller/dto/LivreDTO.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
