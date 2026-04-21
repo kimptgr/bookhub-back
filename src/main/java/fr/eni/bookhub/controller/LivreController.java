f7f8d239dbd4a5895a6cedc6d98265cfaf56face
kimpatingre <kim.patingre@ekolis-eu.com>
2026-04-22 00:11:10 +0200

    réponse http adaptée

--- src/main/java/fr/eni/bookhub/controller/LivreController.java
+++ src/main/java/fr/eni/bookhub/controller/LivreController.java
@@ -2,7 +2,9 @@ package fr.eni.bookhub.controller;
 
 import fr.eni.bookhub.controller.dto.LivreDTO;
 import fr.eni.bookhub.service.LivreService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +16,10 @@ public class LivreController {
     private final LivreService livreService;
 
     @PostMapping
-    public ResponseEntity<Void> ajoutLivre(@RequestBody LivreDTO livreDTO) {
+    public ResponseEntity<Void> ajoutLivre(@Valid @RequestBody LivreDTO livreDTO) {
         livreService.ajoutLivre(livreDTO);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
