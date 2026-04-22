db3185767d25a8359b98b42383fd3d46955179c2
kimptgr <patingrekim@gmail.com>
2026-04-22 13:25:44 +0200

    get etats and get genres and collection bru

--- src/main/java/fr/eni/bookhub/service/EtatService.java
+++ src/main/java/fr/eni/bookhub/service/EtatService.java
@@ -5,7 +5,10 @@ import fr.eni.bookhub.exception.EtatNonExistantException;
 import fr.eni.bookhub.repository.EtatRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,9 @@ public class EtatService {
 
     public Etat retrouveEtat(@NotNull Long id) {
         return etatRepository.findById(id).orElseThrow(() -> new EtatNonExistantException(id.toString()));
+    }
+
+    public @Nullable List<Etat> trouverTousLesEtats() {
+        return etatRepository.findAll();
     }
 }
