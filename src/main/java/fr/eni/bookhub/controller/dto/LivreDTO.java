cba357e851b7b3c3da1877213200b329db778e91
kimptgr <patingrekim@gmail.com>
2026-04-22 09:09:53 +0200

    delete unused import

--- src/main/java/fr/eni/bookhub/controller/dto/LivreDTO.java
+++ src/main/java/fr/eni/bookhub/controller/dto/LivreDTO.java
@@ -5,7 +5,6 @@ import fr.eni.bookhub.validator.Isbn;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.List;
