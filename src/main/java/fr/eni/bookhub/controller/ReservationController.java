80fe09a26ef5264b63d0c478151498a420e78332
kimptgr <patingrekim@gmail.com>
2026-04-24 16:50:09 +0200

    :construction: commencement de la réservation

--- src/main/java/fr/eni/bookhub/controller/ReservationController.java
+++ src/main/java/fr/eni/bookhub/controller/ReservationController.java
@@ -1,11 +1,14 @@
 package fr.eni.bookhub.controller;
 
+import fr.eni.bookhub.controller.dto.ReservationDTO;
+import fr.eni.bookhub.entity.Utilisateur;
 import fr.eni.bookhub.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/reservations")
@@ -14,8 +17,21 @@ public class ReservationController {
 
     private final ReservationService reservationService;
     @PostMapping
-    public ResponseEntity<Void> ajoutReservation() {
-        reservationService.reserverLivre();
+    public ResponseEntity<Void> ajoutReservation(@AuthenticationPrincipal Utilisateur user, @RequestBody ReservationDTO reservationDTO) {
+        //TODO delete quand @AuthenticationPrincipal is implemented
+        Utilisateur utilisateur = null;
+        if (utilisateur == null) {
+            utilisateur = new Utilisateur();
+            utilisateur.setEmail("mtartine@dej.com");
+            utilisateur.setId(1L);
+            utilisateur.setRole(Utilisateur.Role.UTILISATEUR);
+        }
+
+        LocalDate dateReservation = LocalDate.now();
+
+        System.out.println(utilisateur);
+        System.out.println(user);
+        reservationService.reserverLivre(utilisateur, reservationDTO, dateReservation);
         return ResponseEntity.status(201).build();
     }
 }
---
+++ src/main/java/fr/eni/bookhub/controller/dto/ReservationDTO.java
@@ -0,0 +1,4 @@
+package fr.eni.bookhub.controller.dto;
+
+public record ReservationDTO(Long livreId, Long reservateurId) {
+}
