09cc3bbaac082d6d6abe58cd08aedeaabaa319c5
kimpatingre <kim.patingre@ekolis-eu.com>
2026-04-26 13:13:45 +0200

    :ambulance: fix recherhe utilisateur par id et actif

--- src/main/java/fr/eni/bookhub/repository/UtilisateurRepository.java
+++ src/main/java/fr/eni/bookhub/repository/UtilisateurRepository.java
@@ -11,6 +11,6 @@ public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> 
     Optional<Utilisateur> findByEmail(String email);
     boolean existsByEmail(String email);
 
-    Optional<Utilisateur> findByIdAndDesactivateIsFalse(Long id);
+    Optional<Utilisateur> findByIdAndDesactiveIsFalse(Long id);
 
 }
