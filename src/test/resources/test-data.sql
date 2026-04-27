58b6cc0867345f19cdf7747d343430fef5322837
Vincent Monssieur <vincent.monssieur2024@campus-eni.fr>
2026-04-27 10:41:08 +0200

    FIX Enum qui cassait les tests

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,45 @@
+
+insert into utilisateurs (desactive, email, mot_de_passe_chiffre, nom, prenom, role)
+VALUES (false, 'mtartine@dej.com', '$2a$10$26.ezViBvLzeEl1XK5v9L.EUtJ4ApAsYdvNLf7WUjF4QmZuO0rak.', 'Tartine', 'Marie',
+        'UTILISATEUR');
+insert into utilisateurs (desactive, email, mot_de_passe_chiffre, nom, prenom, role)
+VALUES (false, 'admin@mtartine', '$2a$10$26.ezViBvLzeEl1XK5v9L.EUtJ4ApAsYdvNLf7WUjF4QmZuO0rak.', 'Tartine', 'Marie',
+        'BIBLIOTHECAIRE');
+insert into utilisateurs (desactive, email, mot_de_passe_chiffre, nom, prenom, role)
+VALUES (true, 'old@elaff', '$2a$10$26.ezViBvLzeEl1XK5v9L.EUtJ4ApAsYdvNLf7WUjF4QmZuO0rak.', 'Old', 'Compte',
+        'UTILISATEUR');
+
+INSERT INTO auteurs (nom, prenom)
+VALUES ('PERRIN', 'Valérie');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('VIKTOROVITCH', 'Clément');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('LOMBÉ', 'Lisette');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('DESPENTES', 'Virginie');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('MARTIN', 'George R.R.');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('BESSON', 'Philippe');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('GIEBEL', 'Karine');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('DENJEAN', 'Céline');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('FOUCHET', 'Lorraine');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('PUÉRTOLAS', 'Romain');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('LAPIERRE', 'Alexandra');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('MARTIN-LUGAND', 'Agnès');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('CALMEL', 'Mireille');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('COLLETTE', 'Sandrine');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('LOEVENBRUCK', 'Henri');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('GIORDANO', 'Raphaëlle');
+INSERT INTO auteurs (nom, prenom)
+VALUES ('BUSSI', 'Michel');
