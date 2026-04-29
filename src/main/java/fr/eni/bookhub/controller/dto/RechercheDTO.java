d158c2367ef5b3f7d4eeb08d43b6e45f49b9ceb2
Vincent Monssieur <vincent.monssieur2024@campus-eni.fr>
2026-04-29 17:02:00 +0200

    :bug: Fix inutilisables recherche

--- src/main/java/fr/eni/bookhub/controller/dto/RechercheDTO.java
+++ src/main/java/fr/eni/bookhub/controller/dto/RechercheDTO.java
@@ -2,5 +2,5 @@ package fr.eni.bookhub.controller.dto;
 
 import fr.eni.bookhub.entity.Etat;
 
-public record RechercheDTO(String saisie, String[] libellesGenres, Etat.Code libelleEtat) {
+public record RechercheDTO(String saisie, String[] libellesGenres, Etat.Code[] libellesEtats) {
 }
