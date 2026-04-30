50d8fa66852485cd9dc9144c9f0d489a6041cf89
kimptgr <patingrekim@gmail.com>
2026-04-30 13:09:02 +0200

    :tada:

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -108,7 +108,7 @@ INSERT INTO livres_auteurs (id_livre, id_auteur)
 VALUES (2, 2);
 
 INSERT INTO livres (date_de_parution, id_etat, isbn, titre, url_image, synopsis)
-VALUES ('2020-07-10', 3, '9782378801670', 'Brûler brûler brûler',
+VALUES ('2020-07-10', 1, '9782378801670', 'Brûler brûler brûler',
         'https://www.babelio.com/couv/CVT_Bruler-bruler-bruler_1999.jpg', '« Te faire douter.
 Te faire avoir peur.
 Te faire avoir honte
@@ -165,7 +165,7 @@ INSERT INTO livres_auteurs (id_livre, id_auteur)
 VALUES (7, 5);
 
 INSERT INTO livres (date_de_parution, id_etat, isbn, titre, url_image, synopsis)
-VALUES ('2010-01-20', 4, '9782290022177',
+VALUES ('2010-01-20', 1, '9782290022177',
         'Le Trône de Fer - Intégrale tome 4 sur 7',
         'https://www.babelio.com/couv/CVT_Le-Trone-de-Fer-Integrale-tome-4_8756.jpg',
         'Après avoir tué le monarque dément Aerys II Targaryen, Robert Baratheon est devenu le nouveau souverain du royaume des Sept Couronnes. Tandis que, en son domaine de Winterfell, son fidèle ami le duc Eddard Stark rend paisiblement la justice. Mais un jour, le roi Robert lui rend visite, porteur de sombres nouvelles : le trône est en péril. Stark, qui s''est toujours tenu éloigné des affaires du pouvoir, doit alors abandonner les terres du Nord pour rejoindre la cour et ses intrigues. L''heure est grave, d''autant qu''au-delà du Mur qui protège le royaume depuis des siècles, rôdent d''étranges créatures.');
@@ -211,7 +211,7 @@ INSERT INTO livres (date_de_parution,
                     url_image,
                     synopsis)
 VALUES ('2025-11-06',
-        1,
+        3,
         '9782266348942',
         '13 à table !',
         'https://m.media-amazon.com/images/I/41SdgcTkCFL._SX195_.jpg',
@@ -345,6 +345,25 @@ INSERT INTO livres_auteurs VALUES (20, 15);
 INSERT INTO emprunts (date_emprunt, date_retour_effectif, date_retour_previsionnel, id_livre, id_utilisateur) VALUES
     ((CURRENT_DATE - INTERVAL '15 Days'), null, (CURRENT_DATE - INTERVAL '1 Days'),  15, 8);
 
---SCENARIO nouvelle reservation
+--SCENARIO je me retire de la file d'attente mais je m'y remet !
+INSERT INTO utilisateurs (desactive, email, mot_de_passe_chiffre, nom, prenom, role)
+VALUES (false, 'mariecurry@polonium.com', '$2a$10$26.ezViBvLzeEl1XK5v9L.EUtJ4ApAsYdvNLf7WUjF4QmZuO0rak.', 'Curie', 'Marie', 'UTILISATEUR');
 
+INSERT INTO livres (date_de_parution, id_etat, isbn, titre, url_image, synopsis)
+VALUES ('2016-11-06', 2, '9782253087304', 'Mémé dans les orties',
+        'https://www.babelio.com/couv/CVT_Meme-dans-les-orties_2968.jpg',
+        'Ferdinand Brun, 83 ans, solitaire, bougon, acariâtre - certains diraient : seul, aigri, méchant -, s''ennuie à ne pas mourir. Son unique passe-temps ? Éviter une armada de voisines aux cheveux couleur pêche, lavande ou abricot. ');
 
+INSERT INTO auteurs (nom, prenom) VALUES ('VALOGNES', 'Aurélie');
+INSERT INTO livres_genres VALUES (9, 16);
+INSERT INTO livres_auteurs VALUES (21, 16);
+
+insert into reservations (date_disponibilite, date_retrait_max, est_supprimee, rang, date_demande_reservation,
+                          id_livre, id_statut, id_utilisateur)
+values ((CURRENT_DATE - INTERVAL '10 days'), (CURRENT_DATE + interval '4 days'), false, 1,
+        (CURRENT_DATE - INTERVAL '3 Days'), 16, 2, 1);
+
+insert into reservations (date_disponibilite, date_retrait_max, est_supprimee, rang, date_demande_reservation,
+                          id_livre, id_statut, id_utilisateur)
+values ((CURRENT_DATE - INTERVAL '10 days'), (CURRENT_DATE + interval '4 days'), false, 2,
+        (CURRENT_DATE - INTERVAL '3 Days'), 16, 1, 9);
